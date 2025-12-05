02a4547c197c1f3cbd738edde210cfff036560a0
luizodm <luizd.avila@hotmail.com>
2025-12-05 11:13:07 -0300

    feat(api): add GET /grades and POST /grades

---
+++ index.js
@@ -0,0 +1,35 @@
+import http from "http";
+import {v4} from "uuid";
+
+const port = 3000;
+const grades = [];
+
+    const server = http.createServer((request, response)=>{
+    // funções do backend
+    const {method, url} = request;
+    let body = "";
+
+    request.on("data", (chunk) => {
+        body += chunk.toString();
+    });
+
+    request.on("end", () =>
+        {if (url === "/grades" && method === "GET") {
+            response.writeHead(200, {"content-type": "application/json"});
+            response.end(JSON.stringify(grades));
+        } else if (url === "/grades" && method === "POST") {
+            const { studentName, subject, grade } = JSON.parse(body);
+            const newGrade = {id: v4(), studentName, subject, grade };
+            grades.push(newGrade);
+            response.writeHead(201, {"content-type": "application/json"});
+            response.end(JSON.stringify(newGrade));
+        } else {
+            response.writeHead(404, {"content-type": "application/json"});
+            response.end(JSON.stringify({ message: "Route not found"}));
+        }
+    });
+})
+
+server.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
